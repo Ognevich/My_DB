d251003dce884a1f1cdb6bd24dc69f2d4d5b4f63
Bohdan <oleoleqaz@gmail.com>
2025-11-22 16:17:21 +0200

    start working on special command system. detokenize fucntion in parser

--- myDB/specialCommnad.c
+++ myDB/specialCommnad.c
@@ -1,6 +1,28 @@
 #include "specialCommnad.h"
+#include "stdio.h"
+#include <string.h>
+
+#define commandListSize 5
+const char commandCombination[][commandListSize] = { "duti", "abc" };
+
 
 void specialCommand(AppContext* app, const char** argv, int argc)
 {
+	if (argc < 2)
+	{
+		printf("Error: insufficient number of parameters\n");
+		return;
+	}
+
+
+	for (int i = 0; i < argc; i++) {
+		int counter = 1;
+		for (int j = 0; j < commandListSize; j++) {
+			counter++;
+			if (strcmp(argv[counter], commandCombination[i][j]) == 0) {
+
+			}
+		}
+	}
 
 }
