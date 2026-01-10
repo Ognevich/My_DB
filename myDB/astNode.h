2dab97b5cc5377f796d6edc7dd723fa21d9a33ad
Bohdan <oleoleqaz@gmail.com>
2026-01-10 18:27:38 +0200

    create ast struct and base functions

--- myDB/astNode.h
+++ myDB/astNode.h
@@ -1,1 +1,16 @@
-#pragma once
+#ifndef _AST_NODE_H_
+#define _AST_NODE_H_
+
+typedef enum {
+    AST_SELECT = 0,
+    AST_INSERT,
+    AST_DELETE,
+    AST_DROP,
+    AST_UPDATE,
+    AST_WHERE,
+    AST_CONDITION,
+    AST_AND,
+    AST_OR
+} aspNodeType;
+
+#endif;
